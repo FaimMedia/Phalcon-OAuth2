87bf6b7ac2fa30d6fdf82889695921eee34313fe
Tim Peters (FaimMedia B.V.) <tim@faimmedia.nl>
2018-11-06 16:58:48 +0100

    OAuth updates

--- src/Exception/StorageModelException.php
+++ src/Exception/StorageModelException.php
@@ -9,4 +9,6 @@ class StorageModelException extends AbstractException {
 	const INVALID_STORAGE_MODEL = -1;
 	const INVALID_INSTANCE = -2;
 
+	const SAVE_ERROR = -11;
+
 }
--- src/Grant/AbstractGrant.php
+++ src/Grant/AbstractGrant.php
@@ -6,7 +6,11 @@ use FaimMedia\OAuth\Model\Interfaces\BaseInterface;
 
 use Phalcon\Mvc\User\Component as UserComponent;
 
+use Phalcon\Security\Random;
+
 use FaimMedia\OAuth\Di\OAuth;
+
+use FaimMedia\OAuth\Exception\StorageModelException;
 
 abstract class AbstractGrant extends UserComponent {
 
@@ -44,22 +48,45 @@ abstract class AbstractGrant extends UserComponent {
 	 */
 	protected function issueAccessToken() {
 
+	// generate session
 		$sessionModel = $this->getOAuth()->getStorageSession(true);
 		$session = new $sessionModel;
 		$session->assign([
 			'oauthClientId' => $this->getOAuth()->getClient()->getId(),
 			'type'          => 1,
 		]);
-		$session->save();
+		if(!$session->save()) {
+			throw new StorageModelException('Could not generate session', StorageModelException::SAVE_ERROR);
+		}
+
+	// generate access token
+		$random = new Random();
+		$accessTokenHash = $random->base64Safe(40);
 
 		$accessTokenModel = $this->getOAuth()->getStorageAccessToken(true);
 		$accessToken = new $accessTokenModel;
 		$accessToken->assign([
-			'oauthAccessToken' => $accessToken,
+			'accessToken'      => $accessTokenHash,
 			'oauthSessionId'   => $session->getId(),
 			'expireDate'       => null,
 		]);
-		$accessToken->save();
+		if(!$accessToken->save()) {
+			throw new StorageModelException('Could not generate access token', StorageModelException::SAVE_ERROR);
+		}
+
+	// generate refresh token
+		$refreshTokenHash = $random->base64Safe(50);
+
+		$refreshTokenModel = $this->getOAuth()->getStorageRefreshToken(true);
+		$refreshToken = new $refreshTokenModel;
+		$refreshToken->assign([
+			'refreshToken'       => $refreshTokenHash,
+			'oauthAccessTokenId' => $accessToken->getId(),
+			'expireDate'         => null,
+		]);
+		if(!$refreshToken->save()) {
+			throw new StorageModelException('Could not generate refresh token', StorageModelException::SAVE_ERROR);
+		}
 
 	}
 
@@ -76,6 +103,4 @@ abstract class AbstractGrant extends UserComponent {
 	protected function getStorageEngine(string $storageEngine) {
 		return $this->getOAuth()->getStorageEngine($storageEngine);
 	}
-
-
 }
